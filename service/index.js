8408580592cd29890ea325e8ee343af8a8a71693
Lenon Nascimento <lenoninformatica@gmail.com>
2024-10-22 23:33:26 -0300

    Feat: ajuste de api, css, dom

--- service/index.js
+++ service/index.js
@@ -1,3 +1,14 @@
 import fetchData from "../utils/fetchData.js";
 
-fetchData();
+const loadPage = async () => {
+  const data = await fetchData();
+  const title = document.getElementById("title-principal");
+  const job = document.getElementById("profissão");
+  const foto = document.getElementById("foto-pessoal");
+
+  title.innerText = data.nome;
+  job.innerText = data.profissão;
+  foto.src = data.foto;
+};
+
+loadPage();
--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,19 +1,16 @@
-function fetchData() {
-  fetch("../data/infos.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Erro ao carregar o arquivo JSON: " + response.status);
-      }
-      const data = response.json();
-      return data;
-    })
-    .then((data) => {
-      // Aqui você trabalha com os dados JSON no console.log
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error("Erro ao fazer o fetch:", error.message);
-    });
+async function fetchData() {
+  try {
+    const response = await fetch("../data/infos.json");
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error.message);
+  }
 }
 
+fetchData();
+
 export default fetchData;
+
+// fetch("../data/infos.json");
